35a74e3a346979238dc0ca713d7ca46afcd10fa2
Ngo Tu <ngotu@192.168.0.109.non-exists.ptr.local>
2024-11-06 07:59:36 +0700

    Update

--- main.go
+++ main.go
@@ -1,36 +1,43 @@
 package main
 
 import (
+	"demo1/tasks"
 	"log"
+	"net/http"
+	"time"
 
 	"demo1/config"
-	"demo1/tasks"
 	"demo1/worker"
 	"github.com/hibiken/asynq"
 )
 
+var client = asynq.NewClient(config.RedisConfig())
+
 func main() {
-	// Khởi động worker server ở một goroutine khác
 	go worker.StartWorkerServer()
-
-	// Kết nối tới Redis server và tạo client
-	client := asynq.NewClient(config.RedisConfig())
 	defer client.Close()
-
-	// Tạo job gửi email
-	task, err := tasks.NewEmailTask("user@example.com", "Welcome", "Thanks for signing up!")
+	mux := http.NewServeMux()
+	mux.Handle("/hello", http.HandlerFunc(addTask))
+	log.Println("Listening on :8080")
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
-		log.Fatalf("Không thể tạo job gửi email: %v", err)
+		return
 	}
 
-	// Thêm job vào hàng đợi với các tùy chọn
-	info, err := client.Enqueue(task, asynq.Queue("critical"))
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+
+	task, err := tasks.NewEmailTask("user@example.com", "Welcome", "Thanks for signing up!")
+	if err != nil {
+		log.Fatalf("Không thể tạo payload cho job: %v", err)
+	}
+
+	// Tạo một task và thêm vào hàng đợi
+	info, err := client.Enqueue(task, asynq.Queue("critical"), asynq.MaxRetry(3), asynq.Timeout(30*time.Second))
 	if err != nil {
 		log.Fatalf("Không thể thêm job vào hàng đợi: %v", err)
 	}
 
 	log.Printf("Đã thêm job vào hàng đợi với ID: %s", info.ID)
-
-	// Giữ main goroutine hoạt động để worker chạy liên tục
-	select {}
 }
--- worker/worker.go
+++ worker/worker.go
@@ -8,19 +8,16 @@ import (
 	"github.com/hibiken/asynq"
 )
 
-// StartWorkerServer khởi chạy worker server để xử lý các job trong hàng đợi
 func StartWorkerServer() {
-	// Tạo server với cấu hình Redis từ config
 	server := asynq.NewServer(config.RedisConfig(), asynq.Config{
-		Concurrency: 10, // Số lượng worker chạy song song
+		Concurrency: 8,
 	})
-
-	// Đăng ký các handler cho các loại job
 	mux := asynq.NewServeMux()
 	mux.HandleFunc(tasks.EmailTask, tasks.HandleEmailTask)
-
 	// Chạy server để xử lý các job
 	if err := server.Run(mux); err != nil {
 		log.Fatalf("Không thể chạy worker: %v", err)
 	}
+
+	log.Println("Worker start")
 }
